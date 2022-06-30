8f9d3dd2da6c78c73dadc813edd1c0edeb713d91
Kemunto Gift <giftoyugi@gmail.com>
2022-06-30 15:13:48 +0300

    add organization list activity and layout file

---
+++ app/src/main/java/com/pro/fooddonorke/OrganizationListActivity.java
@@ -0,0 +1,14 @@
+package com.pro.fooddonorke;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OrganizationListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_organization_list);
+    }
+}
