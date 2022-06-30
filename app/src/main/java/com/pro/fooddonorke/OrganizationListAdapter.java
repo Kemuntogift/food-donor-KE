0aec16b7db9d8722b5a0cfa356477b3942b0614f
Kemunto Gift <giftoyugi@gmail.com>
2022-06-30 20:03:00 +0300

    add adapter

---
+++ app/src/main/java/com/pro/fooddonorke/OrganizationListAdapter.java
@@ -0,0 +1,17 @@
+package com.pro.fooddonorke;
+
+import android.content.Context;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class OrganizationListAdapter extends RecyclerView.Adapter<OrganizationListAdapter.OrganizationViewHolder> {
+    private List;
+    private Context mContext;
+
+    public OrganizationListAdapter(Context context, List) {
+        mContext = context;
+
+    }
+}
