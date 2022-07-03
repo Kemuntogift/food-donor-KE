f246f8ac49282f08dcd882d0295c02fb4ff9f3f5
Kemunto Gift <giftoyugi@gmail.com>
2022-07-03 18:58:24 +0300

    Bind layout tems to organizationList Activity

--- app/src/main/java/com/pro/fooddonorke/OrganizationListActivity.java
+++ app/src/main/java/com/pro/fooddonorke/OrganizationListActivity.java
@@ -1,14 +1,41 @@
 package com.pro.fooddonorke;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class OrganizationListActivity extends AppCompatActivity {
+    private List<String> charities;
+    @BindView(R.id.searchImage)
+    ImageView mSearchView;
+    @BindView(R.id.searchText)
+    EditText mSearchText;
+    @BindView(R.id.recyclerView)
+    RecyclerView mRecyclerView;
+    @BindView(R.id.errorTextView)
+    TextView mErrorTextView;
+    @BindView(R.id.progressBar)
+    ProgressBar mProgressBar;
 
+    private OrganizationListAdapter mAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_organization_list);
+        ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+        String organization = intent.getStringExtra("organization");
     }
 }
--- app/src/main/java/com/pro/fooddonorke/OrganizationListAdapter.java
+++ app/src/main/java/com/pro/fooddonorke/OrganizationListAdapter.java
@@ -1,17 +1,60 @@
 package com.pro.fooddonorke;
 
 import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class OrganizationListAdapter extends RecyclerView.Adapter<OrganizationListAdapter.OrganizationViewHolder> {
-    private List;
+    private List<String> charities;
     private Context mContext;
 
-    public OrganizationListAdapter(Context context, List) {
+    public OrganizationListAdapter(Context context, List<String> charities) {
         mContext = context;
+    }
 
+    @NonNull
+    @Override
+    public OrganizationViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull OrganizationViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class OrganizationViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.org_image)
+        ImageView mOrganizationImageView;
+        @BindView(R.id.org_name)
+        TextView mNameTextView;
+        @BindView(R.id.org_type)
+        TextView mTypeTextView;
+        @BindView(R.id.org_location)
+        TextView mLocationTextView;
+
+        private Context mContext;
+
+        public OrganizationViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            mContext = itemView.getContext();
+
+        }
     }
 }
