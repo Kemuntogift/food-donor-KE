b15088dbcd8a27445b4b18fd7fbee03932a68f78
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-07-06 12:00:31 +0300

    Update constant

--- app/src/main/java/com/pro/fooddonorke/utilities/Constants.java
+++ app/src/main/java/com/pro/fooddonorke/utilities/Constants.java
@@ -2,7 +2,7 @@ package com.pro.fooddonorke.utilities;
 
 public class Constants {
   public static final String API_BASE_URL = "https://fooddonorkeapi.herokuapp.com/";
-  public static final String FIREBASE_CHILD_DONATIONS = "donations";
+  public static final String FIREBASE_CHILD_DONATIONS = "relief_donations";
   public static final String FIREBASE_CHILD_PROFILE = "profiles";
   public static final String LOCATION_QUERY_PARAMETER = "location";
 }
