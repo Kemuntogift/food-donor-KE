ea5f188a412737c282615606d80c89fa71c37022
Okellothomas <thomasolal33@gmail.com>
2022-07-15 08:07:23 +0300

    add one

--- app/src/main/java/com/pro/fooddonorke/ui/SplashScreen.java
+++ app/src/main/java/com/pro/fooddonorke/ui/SplashScreen.java
@@ -3,7 +3,9 @@ package com.pro.fooddonorke.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -17,6 +19,7 @@ public class SplashScreen extends AppCompatActivity {
     TextView title;
     TextView statement;
     FloatingActionButton arrowButton;
+    SharedPreferences welcome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +31,48 @@ public class SplashScreen extends AppCompatActivity {
             statement = findViewById(R.id.statement);
 
             arrowButton = findViewById(R.id.arrowButton);
-            arrowButton.setOnClickListener(view -> {
-                Intent intent = new Intent(SplashScreen.this, LoginActivity.class);
-                startActivity(intent);
-            });
+//            arrowButton.setOnClickListener(view -> {
+//                Intent intent = new Intent(SplashScreen.this, LoginActivity.class);
+//                startActivity(intent);
+//            });
+
+            // we go ahead and use the handler class
+            Handler handler = new Handler();
+
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+
+                    welcome = getSharedPreferences("welome", MODE_PRIVATE);
+                    boolean isFirstTime = welcome.getBoolean("user", true);
+
+
+                    if (isFirstTime){
+                        // edit the shared preference.
+                        SharedPreferences.Editor editor = welcome.edit();
+                        editor.putBoolean("user", false);
+                        editor.commit();
+                        // we declare the intent of moving from the current activity to the next activity.
+                        Intent intent = new Intent(getApplicationContext(), Onboarding.class);
+
+                        // we call the startActivity method and pass to it out intent.
+                        startActivity(intent);
+
+                        // we call the finish method to destroy or kill the activity.
+                        finish();
+                    }
+                    else {
+                        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+
+                        // we call the startActivity method and pass to it out intent.
+                        startActivity(intent);
+
+                        // we call the finish method to destroy or kill the activity.
+                        finish();
+                    }
+
+                }
+            }, 2000);
 
         }
     }
---
+++ app/src/main/java/com/pro/fooddonorke/ui/sliderAdapter.java
@@ -0,0 +1,97 @@
+package com.pro.fooddonorke.ui;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.viewpager.widget.PagerAdapter;
+
+import com.pro.fooddonorke.R;
+
+
+public class sliderAdapter extends PagerAdapter {
+    // the slider adapter will help us in sliding the images in our application.
+
+    // declare the context variable
+    Context mContext;
+    LayoutInflater layoutInflater;
+
+    // we go ahead and generate the constructor of the class
+
+    public sliderAdapter(Context mContext) {
+        this.mContext = mContext;
+    }
+
+    // We then continue to add images in an array
+
+    int images[] = {
+            R.drawable.faminestriken,
+            R.drawable.street,
+            R.drawable.needy,
+            R.drawable.refugee
+    };
+
+    // we then add the heading array to include the heading in our slides
+    int headings[] = {
+            R.string.first_slide_title,
+            R.string.second_slide_title,
+            R.string.third_slide_title,
+            R.string.fourth_slide_title
+    };
+
+    // we then add the descriptions array to include the descriptions in our slides
+    int descriptions[] = {
+            R.string.first_slide_desc,
+            R.string.second_slide_desc,
+            R.string.third_slide_desc,
+            R.string.fourth_slide_desc
+    };
+
+    // We then go ahead and import the neccessary overidden methods.
+    @Override
+    public int getCount() {
+        // we return the number of slides we will be displaying
+        return headings.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == (ConstraintLayout) object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+
+        // we go ahead to inflate our slider layout
+        layoutInflater = (LayoutInflater) mContext.getSystemService(mContext.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.slider_layout, container, false);
+
+        // we then declare what we have been using.
+
+        de.hdodenhof.circleimageview.CircleImageView imageView = (de.hdodenhof.circleimageview.CircleImageView) view.findViewById(R.id.image_slider);
+//      ImageView image = (ImageView) view.findViewById(R.id.image_slider);
+        TextView desc = (TextView) view.findViewById(R.id.text_desc);
+        TextView title = (TextView) view.findViewById(R.id.text_title);
+
+        // Set the position for the images
+        imageView.setImageResource(images[position]);
+        desc.setText(descriptions[position]);
+        title.setText(headings[position]);
+
+        container.addView(view);
+
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((ConstraintLayout) object);
+    }
+
+}
